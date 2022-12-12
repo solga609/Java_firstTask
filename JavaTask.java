904f547f516dca0af5fc1bb17a56ddca37cf1466
Olga <olga609@mail.ru>
2022-12-12 17:25:23 +0200

    Second_Task

--- JavaTask.java
+++ JavaTask.java
@@ -1,27 +1,38 @@
 import java.util.Scanner;
 
-public class JavaTask {
+// public class JavaTask {
  
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n, m, x, count = 0, sum = 0;
-        System.out.print("Промежуток от: ");
-        n = scan.nextInt();
-        System.out.print("до: ");
-        m = scan.nextInt();
-        System.out.print("Делитель простого числа: ");
-        x = scan.nextInt();
+//     public static void main(String[] args) {
+//         Scanner scan = new Scanner(System.in);
+//         int n, m, x, count = 0, sum = 0;
+//         System.out.print("Промежуток от: ");
+//         n = scan.nextInt();
+//         System.out.print("до: ");
+//         m = scan.nextInt();
+//         System.out.print("Делитель простого числа: ");
+//         x = scan.nextInt();
  
-        System.out.printf("\n В промежутке %d..%d\n", n, m);
-        System.out.printf("Числа, делящиеся на %d: ", x);
-         for (int i = n; i <= m; i++) {
-            if (i % x == 0) {
-                 count++;
-                  sum += i;
-                 System.out.print(i + " ");
-             }
-         }
-        System.out.println("\nКоличество: " + count);
+//         System.out.printf("\n В промежутке %d..%d\n", n, m);
+//         System.out.printf("Числа, делящиеся на %d: ", x);
+//          for (int i = n; i <= m; i++) {
+//             if (i % x == 0) {
+//                  count++;
+//                   sum += i;
+//                  System.out.print(i + " ");
+//              }
+//          }
+//         System.out.println("\nКоличество: " + count);
         
-    }
-}
+//     }
+// }
+
+
+
+
+// 2. Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+
+
+
+
+
